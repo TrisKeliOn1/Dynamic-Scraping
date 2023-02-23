72028c3b312876a931a0d6a54050ce6c2f1ea4a7
Hebert <hebertLandim007@outlook.com>
2023-02-23 16:07:08 -0300

    Search

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ const url ="https://www.mercadolivre.com.br/";
 const searchFor = "macbook";
 
 (async () => {
-    const browser = await pup.launch();
+    const browser = await pup.launch({headless: false});
     const page = await browser.newPage();
+    console.log('iniciei!');
 
     await page.goto(url);
+    console.log('Fui para a URL');
 
+    await page.waitForSelector('#cb1-edit')
+
+    await page.type('#cb1-edit', searchFor);
+
+    await Promise.all([
+        page.waitForNavigation(),
+        await page.click('.nav-search-btn')
+    ])
+
+
+    await page.waitForTimeout(3000);
     await browser.close();
 })();
